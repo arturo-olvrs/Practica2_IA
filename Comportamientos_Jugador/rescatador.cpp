1529a3d9a7e36c8a6b28e443ec1510872d8c1c44
arturo-olvrs <2004.olivares@gmail.com>
2025-04-01 13:31:25 +0200

    Cont hasta pag 12

--- Comportamientos_Jugador/rescatador.cpp
+++ Comportamientos_Jugador/rescatador.cpp
@@ -8,7 +8,7 @@ Action ComportamientoRescatador::think(Sensores sensores)
 	switch (sensores.nivel)
 	{
 	case 0:
-		// accion = ComportamientoRescatadorNivel_0 (sensores);
+		accion = ComportamientoRescatadorNivel_0 (sensores);
 		break;
 	case 1:
 		// accion = ComportamientoRescatadorNivel_1 (sensores);
@@ -32,9 +32,82 @@ int ComportamientoRescatador::interact(Action accion, int valor)
 	return 0;
 }
 
+
+int ComportamientoRescatador::veoCasillaInteresante(Sensores sensores){
+
+	int res = 0;
+
+	// Prioridad:
+	// 1. WALK		(costará un paso)
+	// 2. TURN_SL	(costará un giro + un paso)
+	// 3. TURN_SR 	(costará dos giros + un paso)
+
+	// Iteramos por tanto 2,3,1
+
+	// Comenzamos por identificar puesto base
+	if (sensores.superficie[2] == 'X') res = 2;
+	else if (sensores.superficie[3] == 'X') res = 3;
+	else if (sensores.superficie[1] == 'X') res = 1;
+
+	// Si no hay puesto base y no tengo zapatillas, miro si hay zapatillas
+	if (res == 0 && !tieneZapatillas){
+		if (sensores.superficie[2] == 'D') res = 2;
+		else if (sensores.superficie[3] == 'D') res = 3;
+		else if (sensores.superficie[1] == 'D') res = 1;
+	}
+	
+	// Si no hay puesto base ni zapatillas, miro si hay una casilla de interés
+	if (res == 0){
+		if (sensores.superficie[2] == 'C') res = 2;
+		else if (sensores.superficie[3] == 'C') res = 3;
+		else if (sensores.superficie[1] == 'C') res = 1;
+	}
+
+	return res;
+}
+
+
 Action ComportamientoRescatador::ComportamientoRescatadorNivel_0(Sensores sensores)
 {
 	// El comportamiento de seguir un camino hasta encontrar un puesto base.
+	
+	Action action = IDLE;	// Acción por defecto.
+
+	// Actualización de variables de estado.
+	if (sensores.superficie[0] == 'D')
+		tieneZapatillas = true;
+	
+
+	// Definición del comportamiento
+	if (sensores.superficie[0] == 'X')	// Llegó al puesto base
+		action = Action::IDLE;
+	else if (inTURN_SL){	// Está en su giro SL
+		action = Action::TURN_SR;
+		inTURN_SL = false;
+	}
+	else{ // Vamos a realizar el movimiento determinado por lo que veo
+
+		int res = veoCasillaInteresante(sensores);
+		switch (res){
+			case 0: // No veo nada interesante
+				action = Action::TURN_L;
+				break;
+			case 1: // TURN_SL
+				inTURN_SL = true;
+				action = Action::TURN_L;
+				break;
+			case 2: // WALK
+				action = Action::WALK;
+				break;
+			case 3: // TURN_SR
+				action = Action::TURN_SR;
+				break;
+		}
+
+	}
+
+	lastAction = action;	// Actualizamos la última acción realizada
+	return action;
 }
 
 Action ComportamientoRescatador::ComportamientoRescatadorNivel_1(Sensores sensores)
--- Comportamientos_Jugador/rescatador.hpp
+++ Comportamientos_Jugador/rescatador.hpp
@@ -14,6 +14,9 @@ public:
   ComportamientoRescatador(unsigned int size = 0) : Comportamiento(size)
   {
     // Inicializar Variables de Estado Niveles 0,1,4
+    lastAction = Action::IDLE;
+    tieneZapatillas = false;
+    inTURN_SL = false;
   }
   ComportamientoRescatador(std::vector<std::vector<unsigned char>> mapaR, std::vector<std::vector<unsigned char>> mapaC) : Comportamiento(mapaR,mapaC)
   {
@@ -34,6 +37,22 @@ public:
 
 private:
   // Variables de Estado
+  Action lastAction;      // Almacena la última acción realizada por el rescatador
+  bool tieneZapatillas;   // Indica si el rescatador tiene zapatillas
+  bool inTURN_SL;          // Indica si el rescatador está en su turno de SL
+
+
+  // Métodos Privados
+
+  /**
+   * @brief Método que determina si el rescatador ve una casilla interesante.
+   * 
+   * @return  1 si debe moverse a la casilla 1 (TURN_SL)
+   *          2 si debe moverse a la casilla 2 (WALK)
+   *          3 si debe moverse a la casilla 3 (TURN_SR)
+   *          0 si no ve ninguna casilla interesante. En ese caso, hará TURN_L
+   */
+  int veoCasillaInteresante(Sensores sensores);
 };
 
 #endif
